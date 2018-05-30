625643c70947f3a70387b5060913283865269388
Bulat Niatshin <niatshin@tarantool.org>
2018-05-30 07:28:03 +0300

    fix large queries for exporter

--- config.py
+++ config.py
@@ -1,22 +1,33 @@
 STORAGE_PROMETHEUS = 'http://dashboard.stg.corp.acronis.com:9090'
 QUERY_PARAMS = "{dc='eu6'}"
-DATA_CENTER = 'eu6'
+DATA_CENTERS = [
+    'eu6',
+    'eu7',
+]
 DATASETS_DIR = 'datasets'
 
-TOTAL_SUM = True
+TRAIN_START_TIME = '01/05/2018 00:00'
+TRAIN_END_TIME = '01/05/2018 03:00'
 
-METRICS_PREFIX = 'abgw'
+TEST_START_TIME = '01/05/2018 03:00'
+TEST_END_TIME = '01/05/2018 05:00'
 
-# Metrics which have to be exported from Prometheus
+QUERY_STEP='5m'
+
 METRICS_TO_QUERY = [
-#    'abgw_conns',
-#    'abgw_conns_total',
-#    'abgw_read_reqs_total',
-#    'abgw_read_bytes_total',
-#    'abgw_write_reqs_total',
-#    'abgw_write_bytes_total',
+    ('abgw_req_latency_ms_sum', 'sum', None),
+    ('abgw_iop_latency_ms_count', 'sum', None),
+    ('abgw_iop_latency_ms_sum', 'sum', None),
+    ('abgw_write_reqs_total', 'sum', None),
+    ('abgw_read_reqs_total', 'sum', None),
+    ('abgw_stat_reqs_total', 'sum', None),
+    ('abgw_write_bytes_total', 'sum', None),
+    ('abgw_read_bytes_total', 'sum', None),
+]
+
+RATE_METRICS = [
+    'abgw_conns_total',
     'http_requests_total',
-#    'abgw_iop_latency_ms_count',
 ]
 
 # metrics that somehow are not collected in Prometheus
--- query_metrics.py
+++ query_metrics.py
@@ -3,7 +3,7 @@ import sys, os, time
 import csv, json, argparse
 import config
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from config import STORAGE_PROMETHEUS, QUERY_PARAMS, METRICS_TO_QUERY
 
 import pandas as pd
@@ -28,71 +28,69 @@ def get_metrics_labels(name_regexp='__name__'):
     if status == False:
         return None
 
-    print len(results_json['data'][1:])
+    print('Total amount of metrics: %d' % (len(results_json['data'][1:])))
+
     return results_json['data'][1:-3]
 
-def get_timeseries(metric, start, end, step='1m'):
-    total_query = '{0}/api/v1/query_range'.format(STORAGE_PROMETHEUS)
-    if config.TOTAL_SUM == True:
-        query = 'sum(' + metric + QUERY_PARAMS + ')'
-    else:
-        query = metric + QUERY_PARAMS
+def get_timeseries(metric, start, end, dc_name, step='1m'):
+    assert metric[1] == 'sum' or metric[1] == 'none'
 
-#    print '[get_time_series] total query: ' + total_query
-#    print '[get_time_series] metric query: ' + query
+    total_query = '{0}/api/v1/query_range'.format(STORAGE_PROMETHEUS)
+
+
+    ts_start = int(datetime_to_unix(start))
+    ts_end = int(datetime_to_unix(end))
+
+    if metric[2] != None:
+        query = "%s{dc='%s',%s}" % (metric[0], dc_name, metric[2])
+    else:
+        query = "%s{dc='%s'}" % (metric[0], dc_name)
+
+    if metric[1] == 'sum':
+        query = 'sum(%s)' % query
+
+#    print('[get_time_series] metric query: ' + query)
 
     response = requests.get(total_query,
                             params={
                                     'query': query,
-                                    'start': start,
-                                    'end': end,
+                                    'start': ts_start,
+                                    'end': ts_end,
                                     'step': step
-                                   })
+                                    })
     results_json = response.json()
 
     status = query_status(results_json)
     if status == False:
-        print '[get_time_series] Query had failed: ' + query
-        print 'Exiting right now'
+        print('[get_time_series] Query had failed: ' + query)
+        print('Exiting right now')
         sys.exit()
 
     results = results_json['data']['result']
 
     if len(results) == 0:
-        print metric + ' - unable to extract data'
-        failed_metrics.append(metric)
+        print(metric[0] + ' - unable to extract data')
+        failed_metrics.append(metric[0])
         return None
 
     return results
 
-def verify_args(args):
-    if args.start == None:
-        print 'Start datetime should be specified'
-        sys.exit()
-    if args.end == None:
-        print 'End datetime should be specified'
-        sys.exit()
-
-def datetime_to_unix(date):
-    dt = datetime.strptime(date, "%d/%m/%Y %H:%M")
-
+def datetime_to_unix(dt):
     return time.mktime(dt.timetuple())
 
 def dump_timeseries(results, start, end):
     for ts_json in results:
-#        print '[dump_time_series] dumping json part'
-
         for response in ts_json:
             instance = response['metric']['instance']
             name = response['metric']['__name__']
 
             for gauge in response['values']:
-                print '{1} {0} at [{2}] value - {3}'.format(instance,
+                print('{1} {0} at [{2}] value - {3}'.format(instance,
                                                             name,
                                                             gauge[0],
-                                                            gauge[1])
+                                                            gauge[1]))
 
-            print '-----------'
+            print('-----------')
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -104,91 +102,111 @@ def parse_arguments():
 
     return args
 
-def get_csv_name(name, date):
-    dt = datetime.strptime(date, "%d/%m/%Y %H:%M")
-    csv_path = '{:s}/{:02d}_{:02d}'.format(config.DATASETS_DIR, dt.day,
-                                           dt.month)
-    if not os.path.exists(csv_path):
-        os.mkdir(csv_path)
+def query_one_hour_metric(metric, start, dc_name, step, counter):
+    end = start + timedelta(seconds=60*59) 
+    ts_json = get_timeseries(metric, start, end, dc_name, step)
+    if ts_json == None:
+        return None 
 
-    name = '{:s}/{:s}.csv'.format(csv_path, name)
+    return ts_json
 
-    return name
+def query_metrics(metrics_to_query, start, end, csv_dir, step='1m'):
+    for metric in metrics_to_query:
+        dt_start = datetime.strptime(start, "%d/%m/%Y %H:%M")
+        dt_end = datetime.strptime(end, "%d/%m/%Y %H:%M")
 
-def query_metrics(metrics_to_query, start, end, step='1m'):
+        print('[query_metrics] query: ' + metric[0])
+        dc_name = csv_dir[-3:]
+        counter=0
+        while dt_start < dt_end:
+            ts_json = query_one_hour_metric(metric, dt_start, dc_name, step, counter)
+            if ts_json == None:
+                continue
+
+            query_to_csv(ts_json, metric[0], csv_dir, counter)
+            counter = counter + 1
+            dt_start = dt_start + timedelta(seconds=60*60)
+
+def query_to_csv(results, name, csv_dir, write_counter):
+    file_name = '%s/%s.csv' % (csv_dir, name)
+    csv_file = None
+    if write_counter == 0:
+        csv_file = open(file_name, 'w')
+    else:
+        csv_file = open(file_name, 'a')
+
+    field_names = ['time', name]
+
+    writer = csv.DictWriter(csv_file, fieldnames=field_names) 
+    if write_counter == 0:
+        writer.writeheader()
+
+    for ts_json in results:
+        for gauge in ts_json['values']:
+            time = datetime.fromtimestamp(gauge[0]).strftime('%d_%m_%H:%M')
+            writer.writerow({
+                            'time': time,
+                            name: gauge[1]
+                            })
+
+    csv_file.close()
+
+"""
+def query_metrics(metrics_to_query, start, end, csv_dir, step='1m'):
     results = []
 
-    ts_start = int(datetime_to_unix(start))
-    ts_end = int(datetime_to_unix(end))
-    
     for metric in metrics_to_query:
-        print '[query_metrics] query: ' + metric
-        ts_json = get_timeseries(metric, ts_start, ts_end, step)
+        print('[query_metrics] query: ' + metric[0])
+        dc_name = csv_dir[-3:]
+        ts_json = get_timeseries(metric, start, end, dc_name, step)
         if ts_json == None:
             continue
 
-        query_to_csv(ts_json, metric, start)
+        query_to_csv(ts_json, metric[0], csv_dir)
 
         results.append(ts_json)
 
-    print 'metrics_amount: ' + str(len(results))
-
     return results
 
-def query_to_csv(results, name, start_time):
-#    print '[query_to_csv] writing from json to csv: '
-#    print 'name: ' + name
-
-    file_name = get_csv_name(name, start_time)
+def query_to_csv(results, name, csv_dir):
+    file_name = '%s/%s.csv' % (csv_dir, name)
     csv_file = open(file_name, 'w')
 
-
     field_names = ['time', name]
-    if config.TOTAL_SUM == False:
-        field_names.append('instance')
 
     writer = csv.DictWriter(csv_file, fieldnames=field_names) 
     writer.writeheader()
 
     for ts_json in results:
-#        print json.dumps(ts_json['metric'], indent=4)
-#        print json.dumps(ts_json['values'], indent=4)
-
-        instance = 'DC_SUM'
-        if len(ts_json['metric']) > 0:
-            ''' if metric attibute is not empty,
-                it means that user didn't specified an
-                expr for that query.
-            '''
-            instance = ts_json['metric']['instance']
-#        print 'instance: ' + instance
-
         for gauge in ts_json['values']:
             time = datetime.fromtimestamp(gauge[0]).strftime('%d_%m_%H:%M')
-            if config.TOTAL_SUM == True:
-                writer.writerow({
-                                    'time': time,
-                                    name: gauge[1]
-                                })
-            else:   
-                writer.writerow({
-                                    'instance': instance,
-                                    'dc': config.DATA_CENTER,
-                                    'time': time,
-                                    name: gauge[1]
-                                })
+            writer.writerow({
+                            'time': time,
+                            name: gauge[1]
+                            })
 
     csv_file.close()
-
-def prepare_directories(start_time):
+"""
+def prepare_directories():
     if not os.path.exists(config.DATASETS_DIR):
         os.mkdir(config.DATASETS_DIR)
 
-    dt = datetime.strptime(start_time, "%d/%m/%Y %H:%M")
-    date_path = '{:s}/{:02d}_{:02d}'.format(config.DATASETS_DIR, dt.day, dt.month)
+    train_dirs = []
+    test_dirs = []
 
-    if not os.path.exists(date_path):
-        os.mkdir(date_path)
+    for dc_name in config.DATA_CENTERS:
+        train_dir = '%s/%s' % (config.DATASETS_DIR, 'train_%s' % (dc_name))
+        test_dir = '%s/%s' % (config.DATASETS_DIR, 'test_%s' % (dc_name))
+
+        if not os.path.exists(train_dir):
+            os.mkdir(train_dir)
+        if not os.path.exists(test_dir):
+            os.mkdir(test_dir)
+
+        train_dirs.append(train_dir)
+        test_dirs.append(test_dir)
+
+    return train_dirs, test_dirs
 
 def count_metrics(all_metrics):
     stats_file = open('stat.txt', 'w')
@@ -234,7 +252,6 @@ def compare_with_server(server_metrics):
         local_metrics = local_metrics_file.read().split()
         local_metrics_file.close()
         difference = list(set(local_metrics) - set(server_metrics))
-        print difference
         if len(difference) > 0:
             'WARNING: metrics have changed on server'
             'NEW METRICS: ' + str(difference)
@@ -243,24 +260,27 @@ def compare_with_server(server_metrics):
     return True
 
 if __name__ == '__main__':
-    args = parse_arguments()
-
-    prepare_directories(args.start)
+    train_dirs, test_dirs = prepare_directories()
+    print('train dir: %s' % (str(train_dirs)))
+    print('test dir: %s' % (str(test_dirs)))
 
     metrics = get_metrics_labels()
     count_metrics(metrics)
+
     compare_result = compare_with_server(metrics)
     if compare_result == False:
-        print 'false'
+        print('Server metrics have been updated, exiting right now')
         sys.exit()
 
-    necessary_metrics = [name for name in metrics
-                         if name.startswith(config.METRICS_PREFIX) and
-                         name not in config.BAD_METRICS]
-
-    results = query_metrics(config.METRICS_TO_QUERY, args.start, args.end, '5m')
+    print('-------------------')
+    for train_dir in train_dirs:
+        query_metrics(config.METRICS_TO_QUERY, config.TRAIN_START_TIME,
+                      config.TRAIN_END_TIME, train_dir, step=config.QUERY_STEP)
+    print('-------------------')
+    for test_dir in test_dirs:
+        query_metrics(config.METRICS_TO_QUERY, config.TEST_START_TIME,
+                      config.TEST_END_TIME, test_dir, step=config.QUERY_STEP)
 
     for metric in failed_metrics:
-        print 'Failed: ' + metric
-
+        print('Failed: ' + metric)
 
